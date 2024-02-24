deff4ba56d2b3296727dfd3c3f83775bceb3ff5a
gobst <github@sourcegate.de>
2024-02-24 14:51:32 +0100

    Test

--- scripts/analysis/csfixer.sh
+++ scripts/analysis/csfixer.sh
@@ -15,22 +15,26 @@ if [[ $1 == 'dry-run' ]]; then
     #echo "Checking code style with: PHP Coding Standards Fixer (dry-run)"
     bin/php-cs-fixer fix -v --dry-run --config=cfg/dev/.php-cs-fixer.php > bin/output/phpcsfixer-report.log
     csfixer_status=$?
-    #if [[ $((csfixer_status)) == 0 ]]; then
+    if [[ $((csfixer_status)) == 0 ]]; then
         #echo -e "PHP Coding Standards Fixer result: Success!\n"
-   # else
+        exit 0
+    else
         #echo -e "PHP Coding Standards Fixer result: Failed! See bin/output/phpcsfixer-report.log for details.\n"
-    #fi
+        exit 1
+    fi
 fi
 
 if [[ $1 == 'fix' ]]; then
     #echo "Fix code style with: PHP Coding Standards Fixer"
     bin/php-cs-fixer fix -v --config=cfg/dev/.php-cs-fixer.php > bin/output/phpcsfixer-report.log
     csfixer_status=$?
-    #if [[ $((csfixer_status)) == 0 ]]; then
-    #    #echo -e "PHP Coding Standards Fixer result: Success!\n"
-   # else
-    #    #echo -e "PHP Coding Standards Fixer result: Failed! See bin/output/phpcsfixer-report.log for details.\n"
-    #fi
+    if [[ $((csfixer_status)) == 0 ]]; then
+      #echo -e "PHP Coding Standards Fixer result: Success!\n"
+      exit 0
+    else
+      #echo -e "PHP Coding Standards Fixer result: Failed! See bin/output/phpcsfixer-report.log for details.\n"
+      exit 1
+    fi
 fi
 
-exit $((csfixer_status))
+exit 1
