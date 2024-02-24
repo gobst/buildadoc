7f28f9cb92053e08cf3f801fac79047a5f561829
gobst <github@sourcegate.de>
2024-02-24 17:55:28 +0100

    Test

--- scripts/analysis/psalm.sh
+++ scripts/analysis/psalm.sh
@@ -13,6 +13,8 @@
 
 # Check files with PHP Psalm
 bin/psalm --config=./cfg/dev/psalm.xml --output-format=text > bin/output/psalm-report.log
+cd bin/output
+ls -l
 if [ $? -eq 0 ]; then
     # Psalm result: Success!
     exit 0
