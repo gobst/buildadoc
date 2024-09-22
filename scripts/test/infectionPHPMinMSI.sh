5217cfeeaf9238575b98502818e17bab76fb4b6c
gobst <github@sourcegate.de>
2024-09-22 20:54:24 +0200

    Adjust infectionPHP shellscript

--- scripts/test/infectionPHPMinMSI.sh
+++ scripts/test/infectionPHPMinMSI.sh
@@ -12,4 +12,4 @@
 ############################################################################
 
 # Run mutation tests
-bin/infection --threads=4 --configuration=cfg/dev/infection.json5 --min-msi=95 --git-diff-lines --git-diff-base=main
+bin/infection --threads=4 --configuration=cfg/dev/infection.json5 --min-msi=95 --git-diff-base=main --git-diff-lines
