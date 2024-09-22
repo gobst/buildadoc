e952eed1705a4e40a458e1d727ca1b78be310430
gobst <github@sourcegate.de>
2024-09-22 20:34:00 +0200

    Adjust ci pipeline

---
+++ scripts/test/infectionPHPMinMSI.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+############################################################################
+#
+# This file is part of BuildADoc.
+#
+# (c) Guido Obst
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+
+# Run mutation tests
+bin/infection --threads=4 --configuration=cfg/dev/infection.json5 --min-msi=95 --git-diff-lines
