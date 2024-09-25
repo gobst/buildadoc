cd07b0b5f81217fc26f8069fbaaab3a1f39351c6
gobst <github@sourcegate.de>
2024-09-25 20:43:40 +0200

    Test

--- scripts/test/infectionPHPMinMSI.sh
+++ scripts/test/infectionPHPMinMSI.sh
@@ -11,7 +11,7 @@
 #
 ############################################################################
 
-git fetch all
+git fetch --all
 
 # Run mutation tests
 bin/infection --threads=4 --configuration=cfg/dev/infection.json5 --skip-initial-tests --min-msi=95 --git-diff-base=develop --git-diff-lines --ignore-msi-with-no-mutations --coverage=../../bin/output/unit
