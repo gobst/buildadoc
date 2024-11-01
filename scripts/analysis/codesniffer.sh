ea433f08af3d10d75cc9b80f2326d3bdfa93a9b5
gobst <github@sourcegate.de>
2024-11-01 16:54:44 +0100

    Apply PHPCBF fixes

--- scripts/analysis/codesniffer.sh
+++ scripts/analysis/codesniffer.sh
@@ -13,11 +13,15 @@
 
 # Check files with PHP CodeSniffer and fix the issues if possible
 bin/phpcbf --standard=cfg/dev/phpcs.xml src > bin/output/phpcbf-report.log
-git add .
-git commit -m "Apply PHPCBF fixes"
-git push origin HEAD
+if [ -n "$(git status --porcelain)" ]; then
+  git add .
+  git commit -m "Apply PHPCBF fixes"
+  git push origin HEAD
+fi
+
 bin/phpcs --runtime-set ignore_warnings_on_exit 1 --standard=cfg/dev/phpcs.xml --extensions=php -s -p --report=code --report-width=120 src > bin/output/phpcs-report.log
 phpcs_status=$?
+
 if [[ $phpcs_status == 0 ]]; then
     exit 0
 else
